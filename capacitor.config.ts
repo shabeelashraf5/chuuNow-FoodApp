ccf719c8dfb463462775922b36ee445ce0b29486
shabeelashraf5 <05shebz@gmail.com>
2024-12-10 18:30:07 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chuuNow',
+  webDir: 'www'
+};
+
+export default config;
